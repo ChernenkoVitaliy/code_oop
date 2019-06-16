57f7791587c341f830230aa74826b9f6eba92142
unknown <chernenko_vitaliy@ukr.net>
2019-06-16 16:09:09 +0300

    homework m2_3_t3_done

--- src/com/company/homework/m2_3/task3/Main.java
+++ src/com/company/homework/m2_3/task3/Main.java
@@ -1,6 +1,17 @@
 package com.company.homework.m2_3.task3;
 
-/*Написать аналог Integer.parseint(). Использовать исключения*/
+/*Написать аналог Integer.parseint(). Использовать исключения(+)*/
+
+import com.company.homework.m2_3.task1.FormatException;
 
 public class Main {
+    public static void main(String[] args) {
+        String s = "2012345";
+
+        try {
+            System.out.println(MyInteger.parseInt(s));
+        } catch (FormatException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/company/homework/m2_3/task3/MyInteger.java
+++ src/com/company/homework/m2_3/task3/MyInteger.java
@@ -4,28 +4,42 @@ import com.company.homework.m2_3.task1.FormatException;
 
 public class MyInteger {
 
-    void parseInt(String s) throws FormatException {
+    static int parseInt(String s) throws FormatException {
+        boolean isNegative = false; //флаг для отрицательного числа. Если true значение негативное
+        int result = 0; //если все ОК, в этой переменной будет результат
+        int i = 0;//переменная для цикла. Нужна для того, что бы если первый символ минус, цикл стартовал не с 0, а с 1
 
+        //если приходит null или длинна строки 0, бросаем исключение
         if(s == null || s.length() == 0)
-            throw new FormatException("value can't be NULL");
+            throw new FormatException("value can't be NULL or length == 0");
 
+        //если первый символ 0, меняем флаг на true и счетчик для цикла меняем на 1
+        if (s.charAt(0) == '-') {
+            isNegative = true;
+            i = 1;
+        }
+
+        //разбиваем строку на массив символов
         char[] arr = s.toCharArray();
-        boolean b = false;
-        int result = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 48; j < 58; j++) {
-                if(arr[i] == j) {
-                    b = true;
-                    return;
-                }
-            }
-            if (b == false)
-                throw new FormatException("wrong value");
-            else {
-                //закончить
+        /*Проходим по массиву символов и от каждого символа отнимаеи 48 или символ '0'(нужно для упрощения подсчета.
+         цифры в таблице ASCII начинаются с 48)
+        если после отнимания символ меньше или равно 10 и больше 0, тогда результат
+        умножаем на 10 и добавляем к нему значение из массива*/
+        for (; i < arr.length; i++) {
+            if(arr[i] - '0' <= 10 && arr[i] - '0' > 0){
+                result *= 10;
+                result += arr[i] - '0';
+            }else {
+                throw new FormatException("wrong character: " + arr[i]);
             }
         }
+
+        //тут все ясно и так
+        if (isNegative)
+            result = -result;
+
+        return result;
 
 
     }
