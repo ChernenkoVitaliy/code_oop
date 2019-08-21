c771d7d2eb206cca6e541a5f0798d510dedf7e67
chernenko.v <28041989zxcvVCXZ13724659_>
2019-08-21 18:34:16 +0300

    m2_8_hw_t3

--- src/com/company/homework/m2_8/task3/MainT3M28.java
+++ src/com/company/homework/m2_8/task3/MainT3M28.java
@@ -1,17 +1,31 @@
 package com.company.homework.m2_8.task3;
 
-/*Решить задачу подсчета повторяющихся элементов в массиве с помощью HashMap.*/
+/*Решить задачу подсчета повторяющихся элементов в массиве с помощью HashMap.(+)*/
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class MainT3M28 {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 2, 2, 3};
+        int arr[] = {1, 2, 2, 2, 3, 3, 55, 55, 55};
         Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
 
         for (int i = 0; i < arr.length; i ++) {
+            if (map.containsKey(arr[i])) {
+                if (count == 0)
+                    count = 1;
+                count++;
+            }else {
+                map.put(arr[i], arr[i]);
+            }
         }
+
+        System.out.println(count);
+
+        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext())
+            System.out.println(iterator.next());
     }
 }
